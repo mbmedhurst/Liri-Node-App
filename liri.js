f90057fee3d3b8e5876ee12575520f2adf64cb96
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-18 18:22:35 -0700

    concert api working and response formatted

--- liri.js
+++ liri.js
@@ -3,27 +3,40 @@ const fs = require('fs')
 const axios = require('axios')
 // const keys = require("./keys.js")
 // const spotify = new Spotify(keys.spotify)
-// const spotifyAPI = require('node-spotify-api')
+// const Spotify = require('node-spotify-api')
 // const moment = require('moment')
 // moment().format()
 
-
-// console.log(pArgs)
-
-let movieThis = _ => {
-    const [, , title] = process.argv
-    axios.get(`http://www.omdbapi.com/?t=${title}&apikey=31eeab3a`)
-    .then(r =>  {
-        console.log(`Title: ${r.data.Title}`)
-        console.log(`Year: ${r.data.Year}`),    
-        console.log(`IMDB Rating: ${r.data.Ratings[0].Value}`),
-        console.log(`Rotten Tomatoes Rating: ${r.data.Ratings[1].Value}`),
-        console.log(`Country: ${r.data.Country}`),
-        console.log(`Language: ${r.data.Language}`),
-        console.log(`Plot: ${r.data.Plot}`),
-        console.log(`Cast: ${r.data.Actors}`)
-    })
-    .catch(e => console.log(e))
+let concertThis = _ => {
+    const [, , artist] = process.argv
+    axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
+    .then(r => {
+    for (i = 0; i < r.data.length; i++) {
+    console.log(`Venue: ${r.data[i].venue.name}`)
+    console.log(`Location: ${r.data[i].venue.city}, ${r.data[i].venue.country}`)
+    // use moment to format date as "MM/DD/YYYY")
+    console.log(`Date: ${r.data[i].datetime}`)
 }
-movieThis()
+})
+.catch(e => console.log(e))
+}
+concertThis()
+
+// // still need to add logic if no title is entered
+// let movieThis = _ => {
+//     const [, , title] = process.argv
+//     axios.get(`http://www.omdbapi.com/?t=${title}&apikey=31eeab3a`)
+//     .then(r =>  {
+//         console.log(`Title: ${r.data.Title}`)
+//         console.log(`Year: ${r.data.Year}`),    
+//         console.log(`IMDB Rating: ${r.data.Ratings[0].Value}`),
+//         console.log(`Rotten Tomatoes Rating: ${r.data.Ratings[1].Value}`),
+//         console.log(`Country: ${r.data.Country}`),
+//         console.log(`Language: ${r.data.Language}`),
+//         console.log(`Plot: ${r.data.Plot}`),
+//         console.log(`Cast: ${r.data.Actors}`)
+//     })
+//     .catch(e => console.log(e))
+// }
+// movieThis()
 
