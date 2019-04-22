91c653bb4e975aea05b3a205d3b5772d3fc6fbfa
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-04-22 15:22:29 -0700

    switchcase working for movie and concert

--- liri.js
+++ liri.js
@@ -1,43 +1,60 @@
 require("dotenv").config()
 const fs = require('fs')
 const axios = require('axios')
+// const Spotify = require('node-spotify-api')
 // const keys = require("./keys.js")
 // const spotify = new Spotify(keys.spotify)
-// const Spotify = require('node-spotify-api')
 const moment = require('moment')
 moment().format()
+const [, , action, name] = process.argv
 
+// // Spotify Function
+// spotify
+//   .search({ type: 'track', query: 'All the Small Things' })
+//   .then(r => console.log(r))
+//   .catch(e => console.log(e))
+
+// Bands In Town Function (Mariah Carey is a good one to use)
 let concertThis = _ => {
-    const [, , artist] = process.argv
-    axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
+  axios.get(`https://rest.bandsintown.com/artists/${name}/events?app_id=codingbootcamp`)
     .then(r => {
-    for (i = 0; i < r.data.length; i++) {
-    console.log(`Venue: ${r.data[i].venue.name}`)
-    console.log(`Location: ${r.data[i].venue.city}, ${r.data[i].venue.country}`)
-    // use moment to format date as "MM/DD/YYYY")
-    let date = moment(`${r.data[i].datetime}`).format('MMMM DD, YYYY')
-    console.log(`Date: ${date}`)
+      for (i = 0; i < r.data.length; i++) {
+        console.log(`
+        Venue: ${r.data[i].venue.name}
+        Location: ${r.data[i].venue.city}, ${r.data[i].venue.country}
+        Date: ${moment(`${r.data[i].datetime}`).format('MMMM DD, YYYY')}
+        `)
+      }
+    })
+    .catch(e => console.log(e))
 }
-})
-.catch(e => console.log(e))
+
+// OMDB Function
+// still need to add logic if no title is entered
+let movieThis = _ => {
+  axios.get(`http://www.omdbapi.com/?t=${name}&apikey=31eeab3a`)
+    .then(r => {
+      console.log(`
+      Title: ${r.data.Title}
+      Year: ${r.data.Year}
+      IMDB Rating: ${r.data.Ratings[0].Value}
+      Rotten Tomatoes Rating: ${r.data.Ratings[1].Value}
+      Country: ${r.data.Country}
+      Language: ${r.data.Language}
+      Plot: ${r.data.Plot}
+      Cast: ${r.data.Actors}
+      `)
+    })
+    .catch(e => console.log(e))
 }
-concertThis()
 
-// // still need to add logic if no title is entered
-// let movieThis = _ => {
-//     const [, , title] = process.argv
-//     axios.get(`http://www.omdbapi.com/?t=${title}&apikey=31eeab3a`)
-//     .then(r =>  {
-//         console.log(`Title: ${r.data.Title}`)
-//         console.log(`Year: ${r.data.Year}`)  
-//         console.log(`IMDB Rating: ${r.data.Ratings[0].Value}`)
-//         console.log(`Rotten Tomatoes Rating: ${r.data.Ratings[1].Value}`)
-//         console.log(`Country: ${r.data.Country}`)
-//         console.log(`Language: ${r.data.Language}`)
-//         console.log(`Plot: ${r.data.Plot}`)
-//         console.log(`Cast: ${r.data.Actors}`)
-//     })
-//     .catch(e => console.log(e))
-// }
-// movieThis()
-
+switch (action) {
+  case 'movie-this':
+    movieThis()
+    break
+  case 'concert-this':
+    concertThis()
+    break
+  default:
+    console.log('Invalid argument')
+}
